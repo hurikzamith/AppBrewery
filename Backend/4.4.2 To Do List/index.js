452ef1c15cfadf1d5c788c2f66ba8531ec5a8e99
hurik zamith <hurikzamith@gmail.com>
2023-08-10 13:39:11 -0300

    routes function to do list addItems

---
+++ Backend/4.4.2 To Do List/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let listItems = [];
+
+function addItem(item) {
+  listItems.push(item);
+}
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.post("/submit", (req, res) => {
+  addItem(req.body['item']);
+  res.render('index.ejs', { listItems: listItems });
+  console.log(listItems);
+});
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
