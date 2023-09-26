b6bde07220c3cd5103f2391e8a9800ceef0bb599
hurik zamith <hurikzamith@gmail.com>
2023-09-25 21:21:48 -0300

    calculate function

--- Backend/5.7 Capston project - CRYPTO API use/index.js
+++ Backend/5.7 Capston project - CRYPTO API use/index.js
@@ -6,31 +6,34 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 app.get('/', async (req, res)=>{
   try{
     const result = await axios.get('https://api.n.exchange/en/api/v1/get_price/BTCLTC/');
-    console.log(result.data);
-    res.render('index.ejs', {amount_base: result.data.amount_base, amount_quote: result.data.amount_quote, price: result.data.price, timestamp: result.data.timestamp, type: result.data.type, pair_quote: result.data.quote, pair_base: result.data.pair.base, max_amount_quote: result.data.max_amount_quote, min_amount_quote: result.data.min_amount_quote, max_amount_base: result.data.max_amount_base, min_amount_base: result.data.min_amount_base});
+    // console.log(result.data);
+    res.render('index.ejs', {amount_base: result.data.amount_base, amount_quote: result.data.amount_quote, price: result.data.price, timestamp: result.data.timestamp, type: result.data.type, pair_quote: result.data.pair.quote, pair_base: result.data.pair.base, max_amount_quote: result.data.max_amount_quote, min_amount_quote: result.data.min_amount_quote, max_amount_base: result.data.max_amount_base, min_amount_base: result.data.min_amount_base});
   } catch(error){
     res.status(404).send(error.message);
   }
 });
 
 
-app.get("/", (req, res) => {
-  res.render(`index.ejs`);
+app.post("/amount", async (req, res) => {
+  try {
+    const amount_base = req.body.amount;
+    console.log(amount_base);
+    const response = await axios.get(
+      `https://api.n.exchange/en/api/v1/get_price/BTCLTC/?amount_base=${amount_base}`
+    );
+    console.log(response.data);
+    res.render('index.ejs', {amount_base: response.data.amount_base, amount_quote: response.data.amount_quote, price: response.data.price, timestamp: response.data.timestamp, type: response.data.type, pair_quote: response.data.pair.quote, pair_base: response.data.pair.base, max_amount_quote: response.data.max_amount_quote, min_amount_quote: response.data.min_amount_quote, max_amount_base: response.data.max_amount_base, min_amount_base: response.data.min_amount_base});
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: "No activities that match your criteria.",
+    });
+  }
 });
-
-// axios request to api and get bitcoin quotation
-
-
-// FUNCTIONS: GET BITCOIN QUOTATION to sell and to buy
-// STEPS:
-
-// axios to get data from API
-// Route to pass data to the front end
-// Create a form to get the amount of bitcoin to sell and to buy
-
 
 
 app.listen(3000, () => {
